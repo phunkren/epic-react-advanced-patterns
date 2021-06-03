627af7838be4983212497628faaf7dc3bba1d1e6
Andrew James <contact@ajames.dev>
2021-06-03 21:39:16 +0100

    first commit

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -61,8 +61,12 @@ function UserProvider({children}) {
     storedUser: user,
     user,
   })
-  const value = [state, dispatch]
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+  
+  return (
+    <UserContext.Provider value={[state, dispatch]}>
+      {children}
+    </UserContext.Provider>
+  )
 }
 
 function useUser() {
@@ -74,6 +78,14 @@ function useUser() {
 }
 
 // 🐨 add a function here called `updateUser`
+function updateUser(dispatch, user, updates) {
+  dispatch({type: 'start update', updates})
+
+  userClient.updateUser(user, updates).then(
+    updatedUser => dispatch({type: 'finish update', updatedUser}),
+    error => dispatch({type: 'fail update', error}),
+  )
+}
 // Then go down to the `handleSubmit` from `UserSettings` and put that logic in
 // this function. It should accept: dispatch, user, and updates
 
@@ -97,12 +109,7 @@ function UserSettings() {
 
   function handleSubmit(event) {
     event.preventDefault()
-    // 🐨 move the following logic to the `updateUser` function you create above
-    userDispatch({type: 'start update', updates: formState})
-    userClient.updateUser(user, formState).then(
-      updatedUser => userDispatch({type: 'finish update', updatedUser}),
-      error => userDispatch({type: 'fail update', error}),
-    )
+    updateUser(userDispatch, user, formState)
   }
 
   return (
--- src/exercise/04.js
+++ src/exercise/04.js
@@ -4,25 +4,45 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+function callAll(...fns) {
+  return (...args) => {
+    fns.forEach(fn => {
+      fn && fn(...args)
+    })
+  }
+}
+
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
+  const togglerProps = {'aria-pressed': on, onClick: toggle}
 
-  // 🐨 Add a property called `togglerProps`. It should be an object that has
-  // `aria-pressed` and `onClick` properties.
-  // 💰 {'aria-pressed': on, onClick: toggle}
-  return {on, toggle}
+  function getTogglerProps({onClick, ...options}) {
+    return {
+      ...togglerProps,
+      ...options,
+      onClick: callAll(onClick, toggle),
+    }
+  }
+
+  return {on, toggle, getTogglerProps}
 }
 
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
+  const switchProps = getTogglerProps({on})
+
+  const buttonProps = getTogglerProps({
+    'aria-label': 'custom-button',
+    onClick: () => console.info('onButtonClick'),
+    id: 'custom-button-id',
+  })
+
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...switchProps} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
-        {on ? 'on' : 'off'}
-      </button>
+      <button {...buttonProps}>{on ? 'on' : 'off'}</button>
     </div>
   )
 }
